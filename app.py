ecf4b56c82344e45e61843c3d727aaa24d543871
will20090 <willhuang2027@gmail.com>
2026-05-30 23:04:10 -0400

    add contact page

--- app.py
+++ app.py
@@ -767,5 +767,9 @@ def home():
 def builder():
     return render_template('builder.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
